6750312ede71197cd0873b1703615aa24d706503
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2019-11-15 18:29:40 +0530

    v2.0 breakpoint

--- library/src/main/java/com/sumitkolhe/bitsplash/fragments/dialogs/CreditsFragment.java
+++ library/src/main/java/com/sumitkolhe/bitsplash/fragments/dialogs/CreditsFragment.java
@@ -1,7 +1,9 @@
 package com.sumitkolhe.bitsplash.fragments.dialogs;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -76,6 +78,7 @@ public class CreditsFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        
         MaterialDialog.Builder builder = new MaterialDialog.Builder(getActivity());
         builder.customView(R.layout.fragment_credits, false);
         builder.typeface(TypefaceHelper.getMedium(getActivity()), TypefaceHelper.getRegular(getActivity()));
